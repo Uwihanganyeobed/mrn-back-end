e8bba86583d29b39f7e7bf5343f6e21b88b1a870
Uwihanganyeobed <uwihanganyeobed@gmail.com>
2025-08-29 11:25:32 +0200

    a;ll

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,13 +3,14 @@ import express from "express";
 import { getAllBooks,getBook,
     updateBook,deleteBook,
     createBook } from "../controllers/bookController.js";
+import authProtector from "../middleware/authProtector.js";
 
 const router = express.Router()
 
 router.get('/',getAllBooks)
-router.post('/',createBook)
+router.post('/',authProtector, createBook)
 router.get('/:id',getBook)
-router.put('/:id',updateBook)
-router.delete('/:id',deleteBook)
+router.put('/:id',authProtector,updateBook)
+router.delete('/:id',authProtector,deleteBook)
 
 export default router
